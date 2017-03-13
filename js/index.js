e7a8927fc01e1be11d9282871b40bd32242a7667
Eric Neo <eric.neo@optus.com.au>
2017-03-13 21:15:12 +1100

    Updating endpoint + refactoring

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
-function submitForm(form) {
+function submitForm (form) {
     if (this.validateForm(form)) {
 		var httpRequest = new XMLHttpRequest(),
 			data = new FormData(form);
 
-		if (!httpRequest) {
-			alert('Giving up :( Cannot create an XMLHTTP instance');
-			return false;
-		}
 		httpRequest.onreadystatechange = function () {
 			if (httpRequest.readyState === XMLHttpRequest.DONE) {
 				if (httpRequest.status === 200 && JSON.parse(httpRequest.responseText).success) {
-					this.displaySuccessPanel();
+					this.displayPanel('success');
 				} else {
 					alert('Your information could not be submitted');
 				}
 			}
 		}.bind(this);
-		httpRequest.open('POST', 'https://code-test.titan.analogfolk.com/', true);
+		httpRequest.open('POST', 'https://response-test.titan.analogfolk.com/', true);
 		httpRequest.send(data);
 	} else {
 		alert('Please fill out the form completely.');
 	}
+	return false;
 }
 
 function validateForm (form) {
@@ -28,7 +25,7 @@ function validateForm (form) {
 		formIsValid = true;
 
 	for (var i = 0; i < inputs.length; i++) {
-		if (inputs[i].value === '') {
+		if (!inputs[i].checkValidity()) {
 			formIsValid = false;
 			break;
 		}
@@ -37,10 +34,10 @@ function validateForm (form) {
 	return formIsValid;
 }
 
-function displaySuccessPanel() {
+function displayPanel (panelId) {
 	var tabsPanels = document.querySelectorAll('.subscription-widget .tabs-panel');
 	for (var i = 0; i < tabsPanels.length; i++) {
-		if (tabsPanels[i].getAttribute('id') === 'success') {
+		if (tabsPanels[i].getAttribute('id') === panelId) {
 			tabsPanels[i].style.display = 'block';
 		} else {
 			tabsPanels[i].style.display = 'none';
